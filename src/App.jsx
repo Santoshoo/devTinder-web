71e2750713b2c7a579dab0646036c601367aae71
Santosh <sahosantosh117@gmail.com>
2025-02-09 09:53:11 +0530

    accept or reject the connection

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 import Feed from "./components/Feed";  
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
@@ -22,7 +23,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/connections" element={<Connections />} />
-              <Route path="/requests" element={<Profile />} />
+              <Route path="/requests" element={<Requests />} />
             </Route>
           </Routes>
         </BrowserRouter>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,9 @@ navigate("/login");
     <>
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
+          <Link to="/" className="btn btn-ghost text-xl">
+            DevTinder
+          </Link>
         </div>
         {user && (
           <div className="flex gap-2">
@@ -46,13 +48,16 @@ navigate("/login");
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <Link  to="/profile" className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
                     {/* <span className="badge">New</span> */}
                   </Link>
                 </li>
                 <li>
                   <Link to="/connections">Connections</Link>
+                </li>
+                <li>
+                  <Link to="/requests">Requests</Link>
                 </li>
                 <li>
                   <a onClick={handleLogout}>Logout</a>
