be5fb16198c79d86973c5d04f812cb426b0e98ee
Santosh <sahosantosh117@gmail.com>
2025-03-21 11:53:41 +0530

    chat

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,9 @@
- export const BASE_URL = "/api";
+// export const BASE_URL = "/api";
  //export const BASE_URL = "http://localhost:7777";
 
 
-// export const BASE_URL=
-// location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+export const BASE_URL=
+location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+
+
+
